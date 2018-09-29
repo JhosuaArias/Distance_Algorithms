5983eb9078bb7f900c196287e1f64fae25e6003a
JhosuaArias <jhosuarqui@hotmail.com>
2018-09-29 10:25:40 -0600

    Initial commit

---
+++ src/DistanceCalculator.java
@@ -0,0 +1,14 @@
+public final class DistanceCalculator {
+
+    public static int computeHammingDistance(String string1, String string2){
+        return 0;
+    }
+
+    public static double computeLevenshteinDistance(String string1, String string2){
+        return 0.0;
+    }
+
+    public static double computeBigramsDistance(String string1, String string2) {
+        return 0.0;
+    }
+}
