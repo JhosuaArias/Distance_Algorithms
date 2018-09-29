72e394dceafd85c19ef784ce198871d6072c6a5f
Elías Calderón <eliascr98@outlook.com>
2018-09-29 11:50:44 -0600

    Added the Levenshtein algorithm.

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,2 +1,26 @@
-public class FileHandler {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class FileHandler {
+
+    static List<String[]> readFile(String path) {
+        List<String[]> stringArray = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(path))) {
+
+            stream.forEach( (String line) -> stringArray.add(line.split(",")) );
+
+        } catch (IOException e) {
+            System.err.println("File does not exist or locked");
+            System.exit(99);
+        }
+
+        return stringArray;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        for( String[] line : FileHandler.readFile("./resources/test.txt")) {
+            System.out.println(line[0] + " " + line[1]);
+        }
+
     }
 }
